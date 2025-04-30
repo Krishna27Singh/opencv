9e6597fce349e852a34c3338cf5a1fbed8a8d1ce
Krishna27Singh <krishnasingh97857@gmail.com>
2025-05-01 01:01:31 +0530

    huehue

---
+++ face_recognition.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv 
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+people = ['kendrick_lamar', 'taylor_swift', 'christane_bale']
+# features = np.load('features.npy')
+# lables = np.load('labels.npy')
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read('face_trained.yml')
+
+img = cv.imread('faces/kendrick_lamar/download-1.jpg')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('person', gray)
+
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces_rect:
+    faces_region_of_interest = gray[y:y+h, x:x+w]
+
+    label, confidence = face_recognizer.predict(faces_region_of_interest)
+    print(f"label = {people[label]} with a confidence of {confidence}")
+
+    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), thickness=2)
+    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+
+cv.imshow("Detected face", img)
+     
+cv.waitKey(0)
